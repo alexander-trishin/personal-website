4e699d4ad8d38a1fdb2bc6dc40b1c7da6c987a90
Alexander Trishin <Alexander_Trishin@mail.ru>
2021-09-27 17:00:35 +0300

    Extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@
 
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
-const plugin = require('tailwindcss/plugin');
 
 delete colors.lightBlue;
 
@@ -12,18 +11,25 @@ module.exports = {
     darkMode: false,
     theme: {
         extend: {
+            borderWidth: {
+                3: '3px'
+            },
             colors: {
                 ...colors,
                 current: 'currentColor',
-                transparent: 'transparent'
+                transparent: 'transparent',
+                primary: '#9C84EE'
             },
             fontFamily: {
                 lora: ['Lora', 'serif'],
                 poppins: ['Poppins', 'sans-serif']
+            },
+            minHeight: {
+                lg: '700px'
             }
         },
         screens: {
-            xs: '300px',
+            xs: '360px',
             ...defaultTheme.screens
         }
     },
