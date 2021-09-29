5ebdf2ab178f991acba11d218297d73eb868e48c
Alexander Trishin <Alexander_Trishin@mail.ru>
2021-09-29 18:13:26 +0300

    Add temporary styles for intro section to make typography responsive

--- source/modules/personal/components/Intro/Intro.tsx
+++ source/modules/personal/components/Intro/Intro.tsx
@@ -5,6 +5,8 @@ import { Social } from 'modules/personal/elements';
 
 import BackgroundUrl from '../../assets/images/intro-bg.jpg';
 
+import './Intro.pcss';
+
 const styles: Record<string, CSSProperties> = {
     root: {
         background: `#151515 url(${BackgroundUrl}) no-repeat center bottom`,
@@ -25,21 +27,23 @@ const Intro = () => {
             <div className="text-center z-10">
                 <div className="mx-auto max-w-6xl">
                     <div className="text-white px-4">
-                        <Typography className="text-2xl text-primary uppercase" variant="h5">
+                        <Typography className="font-bold text-primary uppercase" variant="h5">
                             Hello, World.
                         </Typography>
 
-                        <Typography className="text-7xl max-w-5xl mb-2 mx-auto" variant="h1">
+                        <Typography className="font-semibold max-w-5xl mb-2 mx-auto" variant="h1">
                             I&apos;m Alexander Trishin.
                         </Typography>
 
-                        <Typography className="uppercase mb-8">Full-Stack Developer</Typography>
+                        <Typography className="position uppercase mb-8">
+                            Full-Stack Developer
+                        </Typography>
                     </div>
                 </div>
             </div>
 
             <Social
-                className="block absolute left-0 bottom-16 list-disc text-center text-3xl text-white w-full"
+                className="social block absolute left-0 bottom-16 list-disc text-center text-white w-full"
                 size="large"
             />
         </section>
