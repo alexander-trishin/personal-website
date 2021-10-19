e5a97e6a2a5d5371a21d8a059f2b3415ae0d5d9f
Alexander Trishin <Alexander_Trishin@mail.ru>
2021-10-19 16:03:16 +0300

    Enable jit mode for tailwind, update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 delete colors.lightBlue;
@@ -27,6 +26,7 @@ const fontClamp = (
 };
 
 module.exports = {
+    mode: 'jit',
     purge: ['./source/**/*.tsx', './public/index.html'],
     darkMode: false,
     theme: {
@@ -55,16 +55,13 @@ module.exports = {
                 '9xl-fluid': [fontClamp(42, 84), { lineHeight: 1.075, letterSpacing: '-1px' }]
             },
             minHeight: {
+                '0vw': '0vw' /* Safari resize fix */,
                 lg: '720px'
+            },
+            scale: {
+                80: '.8'
             }
-        },
-        screens: {
-            xs: '360px',
-            ...defaultTheme.screens
         }
-    },
-    variants: {
-        extend: {}
     },
     plugins: []
 };
